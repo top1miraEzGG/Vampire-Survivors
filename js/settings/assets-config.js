ba2a51254e43e3cc309f7e772061b9ab8d4ee427
lobotomy66 <tanososi629@gmail.com>
2026-04-26 23:20:07 +0300

    Create assets-config.js

---
+++ js/settings/assets-config.js
@@ -0,0 +1,24 @@
+// ===========================
+// ASSETS CONFIG - Конфигурация ресурсов
+// ===========================
+
+const ASSETS = {
+    images: {
+        player: 'assets/images/player/player.png',
+        playerGun: 'assets/images/player/player_gun.png',
+        
+        enemies: {
+            BAT: 'assets/images/enemies/enemy_bat.png',
+            SKELETON: 'assets/images/enemies/enemy_skeleton.png',
+            ZOMBIE: 'assets/images/enemies/enemy_zombie.png',
+            GHOST: 'assets/images/enemies/enemy_ghost.png',
+            ORC: 'assets/images/enemies/enemy_orc.png',
+            VAMPIRE: 'assets/images/enemies/enemy_vampire.png',
+            DEMON: 'assets/images/enemies/enemy_demon.png',
+            LICH: 'assets/images/enemies/enemy_lich.png'
+        }
+    }
+};
+
+window.ASSETS = ASSETS;
+console.log('[Assets] Конфиг загружен');
